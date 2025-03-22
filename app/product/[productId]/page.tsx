5a2cdbd1401f458a71287aeb3b61e78cf84a0341
augustinaslicys <augustinaslicys@gmail.com>
2025-03-22 23:53:44 +0200

    Fix: added key prop to ProductCard

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/components/Container";
 import ProductDetails from "./ProductDetails";
 import { product } from "@/utils/product";
+import { products } from "@/utils/products";
 
 interface IPrams {
   productId: string;
@@ -10,8 +11,12 @@ interface PageProps {
   params: IPrams;
 }
 
-const Product = ({ params }: PageProps) => {
-  console.log("params:", params);
+const Product = async ({ params }: PageProps) => {
+  const product = products.find((item) => item.id === params.productId);
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
 
   return (
     <div className="p-8">
