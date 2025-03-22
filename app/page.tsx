1ba04e4d451b8eee5c9c721d7c1c47ee5779130b
augustinaslicys <augustinaslicys@gmail.com>
2025-03-22 22:19:40 +0200

    Initial commit with my project files

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import { products } from "@/utils/products";
+import Container from "./components/Container";
+import HomeBanner from "./components/nav/HomeBanner";
+import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
+
+export default function Home() {
+  return (
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+        <div
+          className="grid grid-cols-2 sm:grid-cols-3 lg:gird-cols-4 x1-grid-cols-5
+        2xl:gird-cols-6 gap-8"
+        >
+          {products.map((product: any) => {
+            return <ProductCard data={product} />;
+          })}
+        </div>
+      </Container>
+    </div>
+  );
+}
