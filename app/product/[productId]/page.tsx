b2e51705d8df3022e2399d8da1e0ca11f5bbea23
augustinaslicys <augustinaslicys@gmail.com>
2025-03-23 00:00:54 +0200

    Fix: correct type for params in [productId] dynamic route

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,12 +3,11 @@ import Container from "@/app/components/Container";
 import ProductDetails from "./ProductDetails";
 import { products } from "@/utils/products";
 
-// Correct dynamic route typing from Next.js App Router
-interface Props {
+type Props = {
   params: {
     productId: string;
   };
-}
+};
 
 const ProductPage = ({ params }: Props) => {
   const product = products.find((item) => item.id === params.productId);
